075d4c95339b41008c69fd8da1d8f6415fddc3cb
Your Name <l>
2020-04-06 20:53:39 +0800

    把这个小案例上传到GitHub

---
+++ src/com/dwk/web/servlet/LoginServlet.java
@@ -0,0 +1,63 @@
+package com.dwk.web.servlet;
+
+import com.dwk.domain.User;
+import com.dwk.service.UserService;
+import com.dwk.service.impl.UserServiceImpl;
+import org.apache.commons.beanutils.BeanUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
+@WebServlet("/loginServlet")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.setCharacterEncoding("utf-8");
+
+        HttpSession session = request.getSession();
+
+        Map<String, String[]> map = request.getParameterMap();
+
+        User user = new User();
+        try {
+            BeanUtils.populate(user,map);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(user);
+
+        //5.调用Service查询
+        UserService service = new UserServiceImpl();
+        User loginUser = service.login(user);
+        //6.判断是否登录成功
+        if(loginUser != null){
+            //登录成功
+            //将用户存入session
+            session.setAttribute("user",loginUser);
+            //跳转页面
+            response.sendRedirect(request.getContextPath()+"/index.jsp");
+        }else{
+            //登录失败
+            //提示信息
+            request.setAttribute("login_msg","用户名或密码错误！");
+            //跳转登录页面
+            request.getRequestDispatcher("/login.jsp").forward(request,response);
+
+        }
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
